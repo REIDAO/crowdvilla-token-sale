792ba855899f10aba7645445a0d8545f93f05276
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2017-12-14 16:46:21 +0800

    Updated CrowdvillaTokenSale contract
    - upgraded solidity version
    - updated with the latest flow (with referral code and wallet
    redirection)

--- contracts/CrowdvillaTokenSale.sol
+++ contracts/CrowdvillaTokenSale.sol
@@ -1,128 +1,230 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
-contract CrowdvillaTokenSale {
+import './math/SafeMath.sol';
+import "./tokens/REIDAOMintableBurnableLockableToken.sol";
+import "./tokens/REIDAOMintableToken.sol";
+
+contract CrowdvillaTokenSale is Owners {
+  using SafeMath for uint256;
+  //TODO use safemath for all
 
   uint public totalFund;
   uint public uniqueContributors;
   uint public currentStretchGoal;
-  uint public minContribution = 1 ether;
-  uint public rcvPerEth = 400 * 10**8;
-  uint public reiPerEth = 5 * 10**8;
-  uint public mgmtFeePercentage = 20;
-  uint public saleStartBlock;
+  uint public minContribution;
+  uint public crvPerEth;
+  uint public reiPerEth;
+  uint public mgmtFeePercentage;
   uint public saleEndBlock;
-  uint[] public stretchGoals = [100000 ether, 2500000 ether, 5000000 ether];
+  uint public totalReferralMultisig;
+  uint[] public stretchGoals;
 
-  address public whitelister;
-  mapping (address => bool) public whitelist;
-  mapping (uint => uint) public contributionsPerStretchGoal;
+  address public deployer;
+  address public opsAdmin;
+  address public crowdvillaWallet;
+  address public crvTokenAddr;
+  address public crpTokenAddr;
+  address public reiTokenAddr;
+  address walletREIDAO = 0x19BBe5157ffdf6Efa4C84810e7d2AE25832fF45D;
+  mapping (address => Whitelist) public whitelist;
+  mapping (bytes32 => address) public referralMultisig;
+  mapping (uint => mapping (uint => uint)) public contributionsPerStretchGoal; //earlyRegistrant => stretch-goals => value
   mapping (address => uint) public contributionsPerAddress;
   mapping (address => mapping (uint => uint)) public contributions;
   mapping (address => uint) public contributorIndex;
   mapping (uint => address) public reversedContributorIndex;
+  mapping (address => bool) public tokensCollected;
 
-  event Contribute(address indexed contributor, uint amount);
+  event Contribute(uint blkNo, uint blkTs, address indexed contributor, address indexed tokensale, uint amount, bytes32 referralCode);
+  event Whitelisted(uint blkNo, uint blkTs, address indexed contributor, bool isEarlyRegistrant, bytes32 referralCode);
+  event WhitelistChanged(address indexed _old, address indexed _new);
 
-  enum State { Initial, TokenSale, End, Collection }
+  enum State { TokenSale, End, Collection }
   State public state;
 
-  function CrowdvillaTokenSale() {
-    state = State.Initial;
-    saleStartBlock = 40000000;
-    saleEndBlock   = 45000000;
+  REIDAOMintableBurnableLockableToken crvToken;
+  REIDAOMintableBurnableLockableToken crpToken;
+  REIDAOMintableToken reiToken;
 
-    //TODO add whitelister
-    whitelister = address(0x0);
+  struct Whitelist {
+    bool whitelisted;
+    bool isEarlyRegistrant;
+    bytes32 referralCode;
   }
 
+  function CrowdvillaTokenSale(
+      uint _stretchGoal1,
+      uint _stretchGoal2,
+      uint _stretchGoal3,
+      address _opsAdmin,
+      address _wallet,
+      address _crvTokenAddr,
+      address _crpTokenAddr,
+      address _reiTokenAddr) public {
+    deployer = msg.sender;
+    state = State.TokenSale;
+
+    opsAdmin = address(_opsAdmin);
+    crowdvillaWallet = address(_wallet);
+    crvTokenAddr = address(_crvTokenAddr);
+    crpTokenAddr = address(_crpTokenAddr);
+    reiTokenAddr = address(_reiTokenAddr);
+    crvToken = REIDAOMintableBurnableLockableToken(crvTokenAddr);
+    crpToken = REIDAOMintableBurnableLockableToken(crpTokenAddr);
+    reiToken = REIDAOMintableToken(reiTokenAddr);
+
+    minContribution = 1 ether;
+    crvPerEth = 400 * (10**crvToken.decimals());
+    reiPerEth = 5 * (10**reiToken.decimals());
+    mgmtFeePercentage = 20;
+    saleEndBlock = 5280000; //appox end of Mar 2018
+    stretchGoals = [_stretchGoal1 * 1 ether, _stretchGoal2 * 1 ether, _stretchGoal3 * 1 ether];
+  }
+
+
+  // public - START ------------------------------------------------------------
   function () public payable {
     if (msg.value>0) {
       // for accepting fund
       require(isInWhitelist(msg.sender));
       require(msg.value >= minContribution);
-      require(state == State.Initial || state == State.TokenSale);
+      require(state == State.TokenSale);
 
-      if (state == State.Initial && block.number >= saleStartBlock) {
-        state = State.TokenSale;
-      }
-      if (state == State.TokenSale) {
-        if (block.number >= saleEndBlock) {
-          setEndState();
-          msg.sender.transfer(msg.value);
+      if (block.number >= saleEndBlock) {
+        setEndState();
+        msg.sender.transfer(msg.value);
+      } else {
+        totalFund = totalFund.add(msg.value);
+
+        uint earlyRegistrantIndex = 0;
+        if (whitelist[msg.sender].isEarlyRegistrant) {
+          earlyRegistrantIndex = 1;
+        }
+
+        contributions[msg.sender][currentStretchGoal] = contributions[msg.sender][currentStretchGoal].add(msg.value);
+
+        contributionsPerStretchGoal[earlyRegistrantIndex][currentStretchGoal] = contributionsPerStretchGoal[earlyRegistrantIndex][currentStretchGoal].add(msg.value);
+        contributionsPerAddress[msg.sender] = contributionsPerAddress[msg.sender].add(msg.value);
+        bytes32 referralCode = whitelist[msg.sender].referralCode;
+        logContributeEvent(msg.sender, msg.value, referralCode);
+
+        if (referralCode == bytes32(0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563)) {
+          //no referral code
+          crowdvillaWallet.transfer(msg.value);
         } else {
-          totalFund += msg.value;
-          contributions[msg.sender][currentStretchGoal] += msg.value;
-          contributionsPerStretchGoal[currentStretchGoal] += msg.value;
-          contributionsPerAddress[msg.sender] += msg.value;
-          logContributeEvent(msg.sender, msg.value);
-          if (totalFund >= stretchGoals[currentStretchGoal]) {
+          //referral code exist, sending 95% to our wallet. 5% to multisig with arbiter
+          uint crowdvillaAmount = (msg.value.mul(95)).div(100);
+          crowdvillaWallet.transfer(crowdvillaAmount);
+          referralMultisig[referralCode].transfer(msg.value.sub(crowdvillaAmount));
+        }
+
+        // to increase the currentStrechGoal targetted if the current one has been reached.
+        //  also safe-guard if multiple stretch goals reached with a single contribution.
+        // to end the token sale if it has reached the last stretch goal.
+        for (uint currGoal = currentStretchGoal; currGoal < stretchGoals.length; currGoal++) {
+          if (totalFund >= stretchGoals[currGoal] && currentStretchGoal != stretchGoals.length) {
             currentStretchGoal++;
           }
-
           if (currentStretchGoal == stretchGoals.length) {
             setEndState();
           }
+        }
 
-          if (contributorIndex[msg.sender]==0) {
-            uniqueContributors++;
-            contributorIndex[msg.sender] = uniqueContributors;
-            reversedContributorIndex[uniqueContributors] = msg.sender;
-          }
+        if (contributorIndex[msg.sender]==0) {
+          uniqueContributors++;
+          contributorIndex[msg.sender] = uniqueContributors;
+          reversedContributorIndex[uniqueContributors] = msg.sender;
         }
       }
     } else {
-      require(state == State.Collection);
       // for tokens collection
+      require(state == State.Collection);
+      require(!tokensCollected[msg.sender]);
+      uint promisedCRVToken = getPromisedCRVTokenAmount(msg.sender);
+      require(promisedCRVToken>0);
+      require(crvToken.mint(msg.sender, promisedCRVToken));
+      require(crpToken.mint(msg.sender, promisedCRVToken));
+      require(reiToken.mint(msg.sender, getPromisedREITokenAmount(msg.sender)));
+      tokensCollected[msg.sender] = true;
     }
   }
 
-  function getPromisedCRVTokenAmount() public constant returns (uint) {
+  function getPromisedCRVTokenAmount(address _contributor) public constant returns (uint) {
     uint val;
+
+    uint earlyRegistrantBonus = 0;
+    if (whitelist[_contributor].isEarlyRegistrant)
+      earlyRegistrantBonus = 1;
+
     for (uint i=0; i<=currentStretchGoal; i++) {
-      val += (contributions[msg.sender][i] * rcvPerEth * (100 + ((currentStretchGoal-i) * 10))/100) / 1 ether;
+      val += (contributions[_contributor][i] * crvPerEth * (100 + ((currentStretchGoal-i + earlyRegistrantBonus) * 10))/100) / 1 ether;
     }
     return val;
   }
 
-  function getMgmtFeeTokenAmount() public constant returns (uint) {
+  function getPromisedREITokenAmount(address _contributor) public constant returns (uint) {
     uint val;
+    uint totalEthContributions;
     for (uint i=0; i<=currentStretchGoal; i++) {
-      val += (contributionsPerStretchGoal[i] * rcvPerEth * (100 + ((currentStretchGoal-i) * 10))/100) / 1 ether;
+      totalEthContributions = totalEthContributions.add(contributions[_contributor][i]);
     }
-    uint total = (val * 100) / (100 - mgmtFeePercentage);
-    val = total - val;
+    val = (totalEthContributions.mul(reiPerEth)).div(1 ether);
+
     return val;
   }
 
-  function adminUpdateSaleEndBlock (uint _saleEndBlock) {
-    //TODO add access-control
+  function getREIDAODistributionTokenAmount() public constant returns (uint) {
+    //contributionsPerStretchGoal index 0 is for non-earlyRegistrant
+    //contributionsPerStretchGoal index 1 is for earlyRegistrant
+    uint val;
+    for (uint i=0; i<=currentStretchGoal; i++) {
+      val += (contributionsPerStretchGoal[0][i] * crvPerEth * (100 + ((currentStretchGoal-i) * 10))/100) / 1 ether;
+    }
+    for (i=0; i<=currentStretchGoal; i++) {
+      val += (contributionsPerStretchGoal[1][i] * crvPerEth * (100 + ((currentStretchGoal-i + 1) * 10))/100) / 1 ether;
+    }
+    uint total = (val.mul(100)).div(100 - mgmtFeePercentage);
+    val = total.sub(val);
+    return val;
+  }
+
+  /**
+   * @dev Checks if `_contributor` is in the whitelist.
+   * @param _contributor address The address of contributor.
+   */
+  function isInWhitelist(address _contributor) public constant returns (bool) {
+    return (whitelist[_contributor].whitelisted == true);
+  }
+  // public - END --------------------------------------------------------------
+
+
+  // ownerOnly - START ---------------------------------------------------------
+  function collectREIDAODistribution() public ownerOnly {
+    require(!tokensCollected[walletREIDAO]);
+    uint tokenAmount = getREIDAODistributionTokenAmount();
+    require(crvToken.mint(walletREIDAO, tokenAmount));
+    require(crpToken.mint(walletREIDAO, tokenAmount));
+    tokensCollected[walletREIDAO] = true;
+  }
+
+  function updateSaleEndBlock(uint _saleEndBlock) public ownerOnly {
     saleEndBlock = _saleEndBlock;
   }
 
-  function adminSetSendState() {
-    //TODO add access-control
+  function endTokenSale() public ownerOnly {
     setEndState();
   }
 
-  function setEndState() internal {
-    state = State.End;
+  function startCollection() public ownerOnly {
+    state = State.Collection;
   }
 
   /**
-   * @dev Allows authorized signatories to update `_new` as new whitelister.
-   * @param _whitelister address The address of new whitelister.
+   * @dev Allows owners to update `_opsAdmin` as new opsAdmin.
+   * @param _opsAdmin address The address of new opsAdmin.
    */
-  function adminUpdateWhitelister(address _whitelister) {
-    //TODO add access-control
-    whitelister = _whitelister;
-  }
-
-  /**
-   * @dev Allows whitelister to add `_contributor` to the whitelist.
-   * @param _contributor address The address of contributor.
-   */
-  function addToWhitelist(address _contributor) whitelisterOnly {
-    whitelist[_contributor] = true;
+  function updateOpsAdmin(address _opsAdmin) public ownerOnly {
+    opsAdmin = _opsAdmin;
   }
 
   /**
@@ -130,12 +232,12 @@ contract CrowdvillaTokenSale {
    * @param _old address the old contributor address.
    * @param _new address the new contributor address.
    */
-  function adminUpdateContributorAddress(address _old, address _new) {
-    //TODO add access-control
+  function updateContributorAddress(address _old, address _new) public ownerOnly {
     require (state != State.Collection);
-    removeFromWhitelist(_old);
-    addToWhitelist(_new);
+    whitelist[_new] = Whitelist(whitelist[_old].whitelisted, whitelist[_old].isEarlyRegistrant, whitelist[_old].referralCode);
     uint currentContribution;
+
+    bool contributionFound;
     for (uint i=0; i<=currentStretchGoal; i++) {
       currentContribution = contributions[_old][i];
       if (currentContribution > 0) {
@@ -143,9 +245,52 @@ contract CrowdvillaTokenSale {
         contributions[_new][i] += currentContribution;
         contributionsPerAddress[_old] -= currentContribution;
         contributionsPerAddress[_new] += currentContribution;
-        logContributeEvent(_new, currentContribution);
+        logContributeEvent(_new, currentContribution, whitelist[_old].referralCode);
+
+        contributionFound = true;
       }
     }
+    removeFromWhitelist(_old);
+
+    if (contributionFound) {
+      if (contributorIndex[_new]==0) {
+        uniqueContributors++;
+        contributorIndex[_new] = uniqueContributors;
+        reversedContributorIndex[uniqueContributors] = _new;
+      }
+    }
+    WhitelistChanged(_old, _new);
+  }
+  // ownerOnly - END -----------------------------------------------------------
+
+
+  // opsAdmin - START ----------------------------------------------------------
+  /**
+   * @dev Allows opsAdmin to add `_contributor` to the whitelist.
+   * @param _contributor address The address of contributor.
+   * @param _earlyRegistrant bool If contributor is early registrant (registered before public sale).
+   * @param _referralCode bytes32 The referral code. Empty String if not provided.
+   */
+  function addToWhitelist(address _contributor, bool _earlyRegistrant, bytes32 _referralCode) public opsAdminOnly {
+    whitelist[_contributor] = Whitelist(true, _earlyRegistrant, keccak256(_referralCode));
+    Whitelisted(block.number, block.timestamp, _contributor, _earlyRegistrant, keccak256(_referralCode));
+  }
+
+  /**
+   * @dev Allows opsAdmin to register `_multisigAddr` as multisig wallet address for referral code `_referralCode`.
+   * @param _referralCode bytes32 The referral code. Should not be empty since it should have value.
+   * @param _multisigAddr address The address of multisig wallet.
+   */
+  function registerReferralMultisig(bytes32 _referralCode, address _multisigAddr) public opsAdminOnly {
+    referralMultisig[keccak256(_referralCode)] = _multisigAddr;
+    totalReferralMultisig++;
+  }
+  // opsAdmin - END ------------------------------------------------------------
+
+
+  // internal - START ----------------------------------------------------------
+  function setEndState() internal {
+    state = State.End;
   }
 
   /**
@@ -153,37 +298,23 @@ contract CrowdvillaTokenSale {
    * @param _contributor address The address of contributor.
    */
   function removeFromWhitelist(address _contributor) internal {
-    whitelist[_contributor] = false;
+    whitelist[_contributor].whitelisted = false;
+    whitelist[_contributor].isEarlyRegistrant = false;
   }
 
-  /**
-   * @dev Checks if `_contributor` is in the whitelist.
-   * @param _contributor address The address of contributor.
-   */
-  function isInWhitelist(address _contributor) constant returns (bool) {
-    return (whitelist[_contributor] == true);
+  function logContributeEvent(address _contributor, uint _amount, bytes32 _referralCode) internal {
+    Contribute(block.number, block.timestamp, _contributor, this, _amount, _referralCode);
   }
+  // internal - END ------------------------------------------------------------
 
+
+  // modifier - START ----------------------------------------------------------
   /**
-   * @dev Modifier that throws if sender is not whitelister.
+   * @dev Modifier that throws if sender is not opsAdmin.
    */
-  modifier whitelisterOnly {
-    require(msg.sender == whitelister);
+  modifier opsAdminOnly {
+    require(msg.sender == opsAdmin);
     _;
   }
-
-  function getPromisedREITokenAmount() public constant returns (uint) {
-    uint val;
-    uint totalEthContributions;
-    for (uint i=0; i<=currentStretchGoal; i++) {
-      totalEthContributions += contributions[msg.sender][i];
-    }
-    val = totalEthContributions * reiPerEth / 1 ether;
-
-    return val;
-  }
-
-  function logContributeEvent(address _contributor, uint _amount) internal {
-    Contribute(_contributor, _amount);
-  }
+  // modifier - END ------------------------------------------------------------
 }
