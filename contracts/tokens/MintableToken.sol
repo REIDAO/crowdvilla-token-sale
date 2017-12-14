f32f1f94b5ffbd14ab58fb987aa639cdbb3724f3
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2017-12-14 16:25:12 +0800

    Modified token contracts
    - used solidity 0.4.18
    - added BurnableToken
    - modified MintableToken to different Ownable contract (Owners)

--- contracts/tokens/MintableToken.sol
+++ contracts/tokens/MintableToken.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 
 import './StandardToken.sol';
-import './Ownable.sol';
+import '../ownership/Owners.sol';
 
 
 
@@ -13,7 +13,7 @@ import './Ownable.sol';
  * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
  */
 
-contract MintableToken is StandardToken, Ownable {
+contract MintableToken is StandardToken, Owners {
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
   event MintStarted();
@@ -32,11 +32,11 @@ contract MintableToken is StandardToken, Ownable {
    * @param _amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+  function mint(address _to, uint256 _amount) ownerOnly canMint public returns (bool) {
     totalSupply = totalSupply.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     Mint(_to, _amount);
-    Transfer(0x0, _to, _amount);
+    Transfer(address(0), _to, _amount);
     return true;
   }
 
@@ -44,7 +44,7 @@ contract MintableToken is StandardToken, Ownable {
    * @dev Function to stop minting new tokens.
    * @return True if the operation was successful.
    */
-  function finishMinting() onlyOwner public returns (bool) {
+  function finishMinting() ownerOnly canMint public returns (bool) {
     mintingFinished = true;
     MintFinished();
     return true;
@@ -54,7 +54,7 @@ contract MintableToken is StandardToken, Ownable {
    * @dev Function to start minting new tokens.
    * @return True if the operation was successful.
    */
-  function startMinting() onlyOwner public returns (bool) {
+  function startMinting() ownerOnly public returns (bool) {
     mintingFinished = false;
     MintStarted();
     return true;
