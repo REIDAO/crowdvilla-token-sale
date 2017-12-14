11578b0265208900ac4ec65bf41315836257f4e5
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2017-12-14 16:52:51 +0800

    Updated deployment scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,25 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var CrowdvillaTokenSale = artifacts.require("./CrowdvillaTokenSale.sol");
+var REIToken = artifacts.require("./tokens/REIToken.sol");
+var CRVToken = artifacts.require("./tokens/CRVToken.sol");
+var CRPToken = artifacts.require("./tokens/CRPToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(CRVToken)
+  .then(function() {
+    return deployer.deploy(CRPToken);
+  })
+  .then(function() {
+    return deployer.deploy(REIToken);
+  })
+  .then(function() {
+    return deployer.deploy(
+      CrowdvillaTokenSale,
+      100000, 250000, 500000,
+      "0x0E7a6B35D6f10eE4805Af1E244dF3bF7819e1320",
+      "0x0c99d61b2019d48319C0339F2cE30A2C6f3F4430",
+      CRVToken.address,
+      CRPToken.address,
+      REIToken.address
+      );
+  });
 };
