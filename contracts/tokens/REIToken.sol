2e30e5f167b4b07a71fb141248296c29c847dcea
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2017-12-18 13:13:46 +0800

    Added comments on the timestamps

--- contracts/tokens/REIToken.sol
+++ contracts/tokens/REIToken.sol
@@ -13,12 +13,16 @@ contract REIToken is REIDAOMintableToken {
     uint index;
     uint amount = 200000 * 10**8;
     if (block.timestamp >= 1609459200) {
+      //after 01/01/2021 @ 12:00am (UTC)
       index = 0;
     } else if (block.timestamp >= 1577836800) {
+      //after 01/01/2020 @ 12:00am (UTC)
       index = 1;
     } else if (block.timestamp >= 1546300800) {
+      //after 01/01/2019 @ 12:00am (UTC)
       index = 2;
     } else {
+      //for current assignment
       index = 3;
       amount = 150000 * 10**8;
     }
