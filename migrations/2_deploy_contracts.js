26bf1a853056f71dfbf96175f7741da14d00dabf
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2017-12-28 00:30:57 +0800

    Updated wallet addresses as parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,6 +8,10 @@ var CRPAllocationConfig = artifacts.require("./registries/CRPAllocationConfig.so
 var CRPGenerationConfig = artifacts.require("./registries/CRPGenerationConfig.sol");
 var CRPGeneration = artifacts.require("./CRPGeneration.sol");
 
+var reidaoWallet = "0x19BBe5157ffdf6Efa4C84810e7d2AE25832fF45D";
+var crowdvillaWallet = "0x0c99d61b2019d48319C0339F2cE30A2C6f3F4430";
+var opsAdmin = "0x0E7a6B35D6f10eE4805Af1E244dF3bF7819e1320";
+
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(CRVToken)
   .then(function() {
@@ -20,8 +24,9 @@ module.exports = function(deployer, network, accounts) {
     return deployer.deploy(
       CrowdvillaTokenSale,
       100000, 250000, 500000,
-      "0x0E7a6B35D6f10eE4805Af1E244dF3bF7819e1320",
-      "0x0c99d61b2019d48319C0339F2cE30A2C6f3F4430",
+      opsAdmin,
+      crowdvillaWallet,
+      reidaoWallet,
       CRVToken.address,
       CRPToken.address,
       REIToken.address
