a7c7be98c3b84feaa36a90fe47caf39f9d7de349
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2018-02-06 04:52:00 +0800

    Refactored logic in disbursement schedule
    To improve readability

--- contracts/tokens/REIToken.sol
+++ contracts/tokens/REIToken.sol
@@ -30,41 +30,26 @@ contract REIToken is REIDAOMintableToken {
    *   of the same period.
    */
   function disburseToREIDAO() public ownerOnly {
-    uint index;
     uint amount = 200000 * 10**decimals;
-    bool toMint;
-    if (!tokensReleased[index]) {
+    if (!tokensReleased[0]) {
       //for WAVE 1 - immediate
-      toMint = true;
-    } else {
-      if (block.timestamp >= 1577836800) {
-        //for WAVE 2 - after 01/01/2020 @ 12:00am (UTC)
-        index = 1;
-        if (!tokensReleased[index]) {
-          toMint = true;
-        } else {
-          if (block.timestamp >= 1609459200) {
-            //for WAVE 3 - after 01/01/2021 @ 12:00am (UTC)
-            index = 2;
-            if (!tokensReleased[index]) {
-              toMint = true;
-            } else {
-              if (block.timestamp >= 1640995200) {
-                //for WAVE 4 - after 01/01/2022 @ 12:00am (UTC)
-                index = 3;
-                amount = 150000 * 10**decimals;
-                require(!tokensReleased[index]);
-                toMint = true;
-              }
-            }
-          }
-        }
-      }
+      releaseWave(1, amount);
+    } else if (block.timestamp >= 1577836800 && !tokensReleased[1]) {
+      //for WAVE 2 - after 01/01/2020 @ 12:00am (UTC)
+      releaseWave(2, amount);
+    } else if (block.timestamp >= 1609459200 && !tokensReleased[2]) {
+      //for WAVE 3 - after 01/01/2021 @ 12:00am (UTC)
+      releaseWave(3, amount);
+    } else if (block.timestamp >= 1640995200 && !tokensReleased[3]) {
+      //for WAVE 4 - after 01/01/2022 @ 12:00am (UTC)
+      releaseWave(4, 150000 * 10**decimals);
     }
-    if (toMint) {
-      mint(wallet, amount);
-      tokensReleased[index] = true;
-      WaveReleased(index, amount);
-    }
+  }
+
+  function releaseWave(uint wave, uint amount) internal {
+    assert(1 <= wave && wave <=4);
+    mint(wallet, amount);
+    tokensReleased[wave-1] = true;
+    WaveReleased(wave, amount);
   }
 }
