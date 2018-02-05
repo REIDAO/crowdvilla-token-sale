487af0dd7e7a23a3c587e3e36d52d6d9f935a02d
Hendrik T Tan <24722526+hendrikttan@users.noreply.github.com>
2018-02-06 04:00:37 +0800

    Prevented owners to remove themselves and implemented min owners requirement

--- contracts/ownership/Owners.sol
+++ contracts/ownership/Owners.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.4.18;
 contract Owners {
 
   mapping (address => bool) public owners;
+  uint public ownersCount;
+  uint public minOwnersRequired = 2;
 
   event OwnerAdded(address indexed owner);
   event OwnerRemoved(address indexed owner);
@@ -13,10 +15,12 @@ contract Owners {
    */
   function Owners(bool withDeployer) public {
     if (withDeployer) {
+      ownersCount++;
       owners[msg.sender] = true;
     }
     owners[0x23B599A0949C6147E05C267909C16506C7eFF229] = true;
     owners[0x286A70B3E938FCa244208a78B1823938E8e5C174] = true;
+    ownersCount = ownersCount + 2;
   }
 
   /**
@@ -25,6 +29,7 @@ contract Owners {
    */
   function addOwner(address _address) public ownerOnly {
     owners[_address] = true;
+    ownersCount++;
     OwnerAdded(_address);
   }
 
@@ -32,7 +37,7 @@ contract Owners {
    * @dev removes owner, can only by done by owners only
    * @param _address address the address to be removed
    */
-  function removeOwner(address _address) public ownerOnly {
+  function removeOwner(address _address) public ownerOnly notOwnerItself(_address) minOwners {
     owners[_address] = false;
     OwnerRemoved(_address);
   }
@@ -42,6 +47,16 @@ contract Owners {
    */
   modifier ownerOnly {
     require(owners[msg.sender]);
+    _;
+  }
+
+  modifier notOwnerItself(address _owner) {
+    require(msg.sender != _owner);
+    _;
+  }
+
+  modifier minOwners {
+    require(ownersCount > minOwnersRequired);
     _;
   }
 
